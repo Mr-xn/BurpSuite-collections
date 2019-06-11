c0d8fef72a565a01efcb1e6a8ba8790d85fdd776
mr-xn <mrxn.net@gmail.com>
2019-06-11 15:29:27 +0800

    fix bat CRLF&ANSI

--- Create-Desktop-Link.bat
+++ Create-Desktop-Link.bat
@@ -2,7 +2,7 @@ chcp 936
 REM 设置936编码防止某些中文路径导致批处理失效
 @echo off
 setlocal enabledelayedexpansion
-mode con cols=94 lines=30&color 0a&title 创建Burp Suite一键启动脚本快捷方式
+mode con cols=90 lines=30&color 0a&title 创建Burp Suite一键启动【英文原版】脚本快捷方式
 echo ======================================================
 echo m    m                             mm   m          m
 echo ##  ##  m mm  m   m  m mm          #"m  #  mmm   mm#mm
@@ -16,10 +16,10 @@ echo.
 echo [+] 欢迎各位朋友光临我博客@_@：https://mrxn.net
 echo.
 echo [+] 获得当前路径:%~dp0
-set path=%~dp0Burp_start.bat
+set path=%~dp0Burp_start_en.bat
 echo.
 if exist %path% (
-echo [+] 发现Burp一键启动脚本Burp_start.bat
+echo [+] 发现Burp【英文原版】一键启动脚本Burp_start_en.bat
 echo.
 echo [+] 启动脚本路劲：
 echo.
@@ -27,7 +27,7 @@ echo [+] %path%
 echo.
 goto :creat
 ) else (
-echo [-] 注意,未发现启动脚本Burp_start.bat，请注意是否改名,程序退出... 
+echo [-] 注意,未发现【英文原版】启动脚本Burp_start_en.bat，请注意是否改名,程序退出... 
 echo.
 pause
 exit
@@ -39,12 +39,12 @@ echo.
 rem 设置程序的完整路径(必要)
 set Program=%path%
 rem 设置快捷方式名字(必要)
-set LinkName=Burp_Suite
+set LinkName=Burp_Suite_En
 rem 程序工作路径
 set WorkDir=%~dp0
 rem 设置快捷方式说明
-set Desc=BurpSuite汉化版一键启动
-rem 设置快捷方式图标
+set Desc=BurpSuite【英文原版】一键启动
+rem 设置【英文原版】快捷方式图标
 set icon=%~dp0/img/Goescat-Macaron-Burp-suite.ico
 if not defined WorkDir call:GetWorkDir "%Program%"
 (echo Set WshShell=CreateObject("WScript.Shell"^)
@@ -56,7 +56,7 @@ echo oShellLink.WindowStyle=1
 echo oShellLink.Description="%Desc%"
 echo oShellLink.IconLocation="%icon%"
 echo oShellLink.Save)>makelnk.vbs
-echo [+] 桌面快捷方式创建成功!!
+echo [+] 【英文原版】桌面快捷方式创建成功!!
 echo.
 makelnk.vbs
 del /f /q makelnk.vbs
